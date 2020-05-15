fd873b59d15c9ab5821202798d97a76101b54134
Tamanpreet Singh <tamanpreet.singh@nineleaps.com>
2020-05-16 04:31:25 +0530

    Cleared search value

--- src/modules/layout/Header.js
+++ src/modules/layout/Header.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { withRouter } from "react-router-dom";
 import LOGO from "../../globals/assets/images/logo.png";
 import { Menu, Dropdown, Avatar, Input } from "antd";
@@ -8,6 +8,7 @@ import { FEATURE } from "../../globals/config/urlMapping";
 
 const RenderHeader = (props) => {
   const { history } = props;
+  const [value, setvalue] = useState();
 
   const handleSearch = (value) => {
     if (value)
@@ -26,13 +27,21 @@ const RenderHeader = (props) => {
       <div style={{ marginLeft: "10px" }}>
         <img
           className="logo"
-          onClick={() => history.push("/")}
+          onClick={() => {
+            setvalue();
+            history.push("/");
+          }}
           src={LOGO}
           alt="LOGO"
         />
       </div>
       <Input.Group compact style={{ display: "inline", width: "180px" }}>
-        <Input.Search onSearch={handleSearch} placeholder="Search" />
+        <Input.Search
+          onChange={(e) => setvalue(e.target.value)}
+          value={value}
+          onSearch={handleSearch}
+          placeholder="Search"
+        />
       </Input.Group>
       <div style={{ marginRight: "10px" }}>
         <Dropdown overlay={menu} placement="bottomLeft">
