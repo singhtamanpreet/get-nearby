a5429604d9d92cbf74ed1b1283c8e49667c3333c
Tamanpreet Singh <tamanpreet.singh@nineleaps.com>
2020-05-15 18:34:19 +0530

    Added scroll in content and beautification

--- src/modules/layout/index.js
+++ src/modules/layout/index.js
@@ -15,35 +15,65 @@ export const GRLayout = (props) => {
     <div>
       <Layout className="layout">
         <RenderHeader />
-        <Content style={{ padding: "0 50px" }}>
-          <div className="site-layout-content gr-layout-content">
-            <div>
-            <Button type="primary" block style={{ marginTop: '30px', marginBottom: '30px'}}>
-                CHOOSE YOUR LOCATION
-           </Button>
-           <div>
-           <Row className="main-row">
-           <Card sm={6} style={{ width: 150, height: 150 }} >
-             <p>Medical Stores</p>
-             <p>59 stores</p>
-           </Card>
-           <Card sm={6} style={{ width: 150, height: 150 }}>
-             <p>Medical Stores</p>
-             <p>9 stores</p>
-           </Card>
-           <Card sm={6} style={{ width: 150, height: 150 }} >
-             <p>Vegetable stores</p>
-             <p>19 stores</p>
-           </Card>
-           </Row>
-           </div>
-           <Card title="Precautions for COVID-19" className="custom-card" bordered={false}>
-               <p>Don’t go out until its really necessary</p>
-               <p>Wash your hands regularly with soap</p>
-               <p>Maintain distance from other</p>
-               <p>Cover your face while going out, sneezing, coughing, etc</p>
-               <p>Spread awareness</p>
-          </Card>
+        <Content style={{ overflow: "auto", margin: "20px 0" }}>
+          <div style={{ margin: "0 50px" }}>
+            <div className="site-layout-content gr-layout-content">
+              <div>
+                <Button type="primary" block>
+                  CHOOSE YOUR LOCATION
+                </Button>
+                <br />
+                <br />
+                <div>
+                  <Row className="main-row" gutter={[12, 12]}>
+                    <Col>
+                      <Card
+                        hoverable
+                        sm={6}
+                        style={{ width: 150, height: 150 }}
+                      >
+                        <p>Medical Stores</p>
+                        <p>59 stores</p>
+                      </Card>
+                    </Col>
+                    <Col>
+                      <Card
+                        hoverable
+                        sm={6}
+                        style={{ width: 150, height: 150 }}
+                      >
+                        <p>Medical Stores</p>
+                        <p>9 stores</p>
+                      </Card>
+                    </Col>
+                    <Col>
+                      <Card
+                        hoverable
+                        sm={6}
+                        style={{ width: 150, height: 150 }}
+                      >
+                        <p>Vegetable stores</p>
+                        <p>19 stores</p>
+                      </Card>
+                    </Col>
+                  </Row>
+                </div>
+                <br />
+                <br />
+                <Card
+                  title="Precautions for COVID-19"
+                  className="custom-card"
+                  bordered={false}
+                >
+                  <p>Don’t go out until its really necessary</p>
+                  <p>Wash your hands regularly with soap</p>
+                  <p>Maintain distance from other</p>
+                  <p>
+                    Cover your face while going out, sneezing, coughing, etc
+                  </p>
+                  <p>Spread awareness</p>
+                </Card>
+              </div>
             </div>
           </div>
         </Content>
