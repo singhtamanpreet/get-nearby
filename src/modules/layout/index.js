872786145f52c295f186181d776e88bafe32f4a9
Tamanpreet Singh <tamanpreet.singh@nineleaps.com>
2020-05-15 18:14:34 +0530

    Added search and footer

--- src/modules/layout/index.js
+++ src/modules/layout/index.js
@@ -1,11 +1,11 @@
 import React from "react";
 import { Layout } from "antd";
 import { RenderHeader } from "./Header";
-import { Button } from 'antd';
-import { Card } from 'antd';
-import { Row, Col } from 'antd';
-import { Link } from 'react-router-dom';
-import { Typography } from 'antd';
+import { Button } from "antd";
+import { Card } from "antd";
+import { Row, Col } from "antd";
+import { Link } from "react-router-dom";
+import { Typography } from "antd";
 
 const { Text } = Typography;
 const { Content, Footer } = Layout;
@@ -18,35 +18,46 @@ export const GRLayout = (props) => {
         <Content style={{ padding: "0 50px" }}>
           <div className="site-layout-content gr-layout-content">
             <div>
-            <Button type="primary" block style={{ marginTop: '100px', marginBottom: '100px'}}>
+              <Button
+                type="primary"
+                block
+                //style={{ marginTop: '100px', marginBottom: '100px'}}
+              >
                 CHOOSE YOUR LOCATION
-           </Button>
-           <Card title="Precautions for COVID-19" bordered={false} style={{ width: 300, background: 'orange' }}>
-               <p>Don’t go out until its really necessary</p>
-               <p>Wash your hands regularly with soap</p>
-               <p>Maintain distance from other</p>
-               <p>Cover your face while going out, sneezing, coughing, etc</p>
-               <p>Spread awareness</p>
-          </Card>
+              </Button>
+              <br />
+              <br />
+
+              <Card
+                title="Precautions for COVID-19"
+                bordered={false}
+                className="custom-card"
+              >
+                <p>Don’t go out until its really necessary</p>
+                <p>Wash your hands regularly with soap</p>
+                <p>Maintain distance from other</p>
+                <p>Cover your face while going out, sneezing, coughing, etc</p>
+                <p>Spread awareness</p>
+              </Card>
             </div>
           </div>
         </Content>
-        <Footer style={{alignItems: 'center', background: '#335072'}}>
-        <Row className="main-row">
+        <Footer style={{ alignItems: "center" }} className="layout-footer">
+          <Row className="main-row">
             <Col sm={6} sm={12} xs={24}>
-                <Text className="text-about"> © GetNearBy, 2020</Text>
+              <Text className="text-about"> © GetNearBy, 2020</Text>
             </Col>
             <Col sm={6} sm={12} xs={24}>
-                <Link to="/">  Terms and Policies </Link>
+              <Link to="/"> Terms and Policies </Link>
             </Col>
             <Col sm={6} sm={12} xs={24}>
-                <Link to="/">  FAQs </Link>
+              <Link to="/"> FAQs </Link>
             </Col>
             <Col sm={6} sm={12} xs={24}>
-                <Link to="/"> contact@GetNearBy.com </Link>
+              <Link to="/"> contact@GetNearBy.com </Link>
             </Col>
-        </Row>
-    </Footer>
+          </Row>
+        </Footer>
       </Layout>
     </div>
   );
