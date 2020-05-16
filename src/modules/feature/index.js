c009147384a7ba25a665ee7be23aa67bc8bd4ae5
Tamanpreet Singh <tamanpreet.singh@nineleaps.com>
2020-05-16 14:53:55 +0530

    Fixed feature page

--- src/modules/feature/index.js
+++ src/modules/feature/index.js
@@ -5,7 +5,7 @@ import OPEN from "../../globals/assets/images/open-store.png";
 import CLOSE from "../../globals/assets/images/close-store.png";
 import { FEATURE_HEADER_MAPPING } from "../../globals/config/constants";
 import { CustomCard } from "../../globals/partials/atoms/CustomCard";
-import { Empty } from "antd";
+import { Empty, Row, Col } from "antd";
 import { DETAILS } from "../../globals/config/urlMapping";
 
 export const Feature = (props) => {
@@ -15,12 +15,12 @@ export const Feature = (props) => {
   const isSearch = params.feature === "search" ? true : false;
   useEffect(() => {
     if (!isSearch) {
-      Axios.get(`http://localhost:8080/store/getAll?pin=${params.param}`).then(
-        (res) => {
-          const data = res.data;
-          setdata(data);
-        }
-      );
+      Axios.get(
+        `http://localhost:8080/store/getAll?pin=${params.param}&&storeType=${params.feature}`
+      ).then((res) => {
+        const data = res.data;
+        setdata(data);
+      });
     } else {
       Axios.get(`http://localhost:8080/store/search/${params.param}`).then(
         (res) => {
@@ -29,7 +29,8 @@ export const Feature = (props) => {
         }
       );
     }
-  }, [isSearch, params.param]);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
 
   return (
     <div>
@@ -37,18 +38,20 @@ export const Feature = (props) => {
       {data.map((item) => {
         const id = item._id;
         let isOpen = item.status === "open" ? true : false;
+        let status = item.status;
         if (isSearch) {
           const itemSearched = params.param;
           const itemArray = item.items.filter(
             (subItem) => subItem.name === itemSearched
           );
           if (itemArray.length > 0) {
+            status = itemArray[0].status;
             isOpen = itemArray[0].status === "available" ? true : false;
           } else {
             isOpen = false;
           }
         }
-        const { distance, address, location, storeType } = item;
+        const { distance, address, location, storeName } = item;
         const icon = isOpen ? OPEN : CLOSE;
 
         return (
@@ -61,25 +64,40 @@ export const Feature = (props) => {
               )
             }
             key={id}
-            icon={<img className="icon" src={icon} alt={item.status} />}
+            icon={
+              <Row type="flex" justify="center">
+                <img className="icon" src={icon} alt={item.status} />
+                <Col span={24}>
+                  <p style={{ display: "flex", justifyContent: "center" }}>
+                    {status}
+                  </p>
+                </Col>
+              </Row>
+            }
             disabled={!isOpen}
           >
             <div>
-              <h4>{storeType}</h4>
+              <h4>{storeName}</h4>
               <h5>Distance: {distance}</h5>
               <p>
                 Address: {location}, {address}
               </p>
+              <h4>Zone:</h4>
+              <Row type="flex" align="middle">
+                <Col span={8}>Green:</Col>
+                <Col span={12}>
+                  <div className="green-line" />
+                </Col>
+              </Row>
             </div>
           </CustomCard>
         );
       })}
       {data.length === 0 && (
         <div>
-          <br />
-          <br />
-          <br />
-          <Empty image={Empty.PRESENTED_IMAGE_SIMPLE} />
+          <CustomCard>
+            <Empty image={Empty.PRESENTED_IMAGE_SIMPLE} />
+          </CustomCard>
         </div>
       )}
     </div>
