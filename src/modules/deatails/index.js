b2c3f6aa3816553bdfc5bfab7a3745a63af9d611
Tamanpreet Singh <tamanpreet.singh@nineleaps.com>
2020-05-16 15:40:22 +0530

    Added zones in features

--- src/modules/deatails/index.js
+++ src/modules/deatails/index.js
@@ -1,40 +1,20 @@
 import React, { useState } from "react";
 import { useEffect } from "react";
-import { Button } from 'antd';
-import { Select } from 'antd';
 import Axios from "axios";
 
 export const Details = (props) => {
   const { match } = props;
   const { params } = match;
-  const [slot, selectedSlot] = useState([]);
+  const [data, setdata] = useState([]);
   useEffect(() => {
-      Axios.get(`http://localhost:8080/store/getAll?pin=${params.param}`).then(
-        (res) => {
-          const slot = res.data.map((item) => {
-            return(
-              item.timeSlot.map((item1)=> {
-                return(
-                  item1.slot
-                )
-              })
-              )
-          });
-          selectedSlot(slot);
-        }
-      )}, [params.param]);
+    Axios.get(`http://localhost:8080/store/getAll?pin=${params.param}`).then(
+      (res) => {
+        const data = res.data;
+        setdata(data);
+      }
+    );
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
 
-  function handleChange(value) {
-    console.log(`selected ${value}`);
-  }
-  console.log(slot, )
-  return <div>
-    Please Select your time slot
-    <div style={{display: 'flex', flexDirection: 'row', alignItems: 'center', justifyContent: 'space-between' }}>
-    <Select defaultValue="Slots" style={{ width: 250 }} onChange={handleChange}>
-      {slot.map((item, index) => <Select.Option value={item.slot} key={index}>{item.slot}</Select.Option>)}}
-    </Select>
-    <Button type="primary">Apply</Button>
-  </div>,
-  </div>;
+  return <div>I am details page</div>;
 };
